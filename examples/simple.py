579d99a50942030b31af835a92f6f57eb0cb55aa
Davide Magrin <magrin.davide@gmail.com>
2018-05-22 17:54:47 +0200

    Add example and parameter space creation

--- examples/simple.py
+++ examples/simple.py
@@ -1,27 +1,51 @@
 # This is a simple example of how to use the ns-3 SimulationExecutionManager
 
 from sem import CampaignManager
+from itertools import product
+import os
+from pathlib import Path
 
-# Campaign creation
-###################
 
-param_space = {
-    'tcpVariant': ["TcpHybla", "TcpHighSpeed", "TcpHtcp", "TcpVegas",
-                   "TcpScalable", "TcpVeno", "TcpBic", "TcpYeah",
-                   "TcpIllinois", "TcpWestwood", "TcpWestwoodPlus",
-                   "TcpLedbat"],
-    'runs': 10
+# Define campaign parameters
+############################
+
+script = 'wifi-tcp'
+ns_path = "/home/davide/Work/ns-3-dev-gsoc/"
+filename = "/tmp/wifi-tcp-sims.json"
+
+# Create campaign
+#################
+
+# if (Path(filename).exists()):
+#     os.remove(filename)
+# campaign = CampaignManager.new(ns_path, script, filename)
+campaign = CampaignManager.load(filename)
+
+print(campaign)
+
+# Run some simulations
+######################
+
+param_ranges = {
+    'payloadSize': [1472/2, 1472],
+    'dataRate': ['100Mbps'],
+    'tcpVariant': ['TcpHybla', 'TcpHighSpeed', 'TcpHtcp', 'TcpVegas',
+                   'TcpScalable', 'TcpVeno', 'TcpBic', 'TcpYeah',
+                   'TcpIllinois', 'TcpWestwood', 'TcpWestwoodPlus',
+                   'TcpLedbat'],
+    'phyRate': ['HtMcs7'],
+    'simulationTime': [4],
+    'pcap': ['false'],
+    'runs': [10]
 }
 
-config = {
-    'script': 'wifi-tcp',
-    'ns-3-path': "/path/to/ns-3/",
-}
+param_space = [dict(zip(param_ranges, v)) for v in
+               product(*param_ranges.values())]
 
-campaign = CampaignManager.new_from_config(config, 'example-campaign.json')
+# Visualize parameter space
+for param in param_space:
+    print(param)
 
-campaign.run_simulations(param_space)
+# campaign.run_simulations(param_space)
 
-campaign.get_results_as_numpy_array(param_space)
-
-campaign.print_campaign_information()
+# campaign.get_results_as_numpy_array(param_space)
