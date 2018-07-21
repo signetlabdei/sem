ceb2881552aee8a2cbc7a88bf68c76559283c909
Davide Magrin <magrin.davide@gmail.com>
2018-07-21 12:09:38 +0200

    Add command line interface facilities

--- sem/__init__.py
+++ sem/__init__.py
@@ -3,14 +3,9 @@ from .runner import SimulationRunner
 from .parallelrunner import ParallelRunner
 from .database import DatabaseManager
 from .utils import list_param_combinations
-import click
+from .cli import cli
 
 __all__ = ('CampaignManager', 'SimulationRunner', 'ParallelRunner',
            'DatabaseManager', 'list_param_combinations')
 
 name = 'sem'
-
-
-@click.command()
-def main():
-    print("ns-3 Simulation Execution Manager")
