445dd6fc848313091ae01d42cee5493ea28930e5
Davide Magrin <magrin.davide@gmail.com>
2018-07-28 16:43:27 +0200

    Add ability to read parameters from file and tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,6 +1,8 @@
 import sem
 # For testing the command line we leverage click facilities
 from click.testing import CliRunner
+import re
+import pytest
 
 
 def test_cli_help():
@@ -17,10 +19,37 @@ def test_cli_run(tmpdir, ns_3_compiled, config):
                   catch_exceptions=False)
 
 
-def test_cli_view(tmpdir, ns_3_compiled, config):
-    # NOTE For these tests, we rely on the fact that fixture dictionaries are
-    # ordered, in order to support Python 3.4 and 3.5.
+def test_parameters_from_file(tmpdir, ns_3_compiled, config):
+    runner = CliRunner()
 
+    # Write parameter specification to file
+    param_file = str(tmpdir.join("parameters.txt"))
+    with open(param_file, 'w') as f:
+        lines = [
+            "dict: '/usr/share/dict/web2'\n",
+            "time: [False, True]"
+        ]
+        f.writelines(lines)
+
+    # Run some simulations
+    runner.invoke(sem.cli,
+                  ['run',
+                   '--ns-3-path=%s' % ns_3_compiled,
+                   '--results-dir=%s' % tmpdir.join('results'),
+                   '--script=%s' % config['script'],
+                   '--parameters=%s' % param_file],
+                  input='1\n',  # Specify the runs
+                  catch_exceptions=False)
+
+    # View results
+    runner.invoke(sem.cli,
+                  ['view',
+                   '--results-dir=%s' % tmpdir.join('results'),
+                   '--parameters=%s' % param_file],
+                  catch_exceptions=False)
+
+
+def test_cli_workflow(tmpdir, ns_3_compiled, config):
     runner = CliRunner()
 
     # Run some simulations
@@ -30,9 +59,26 @@ def test_cli_view(tmpdir, ns_3_compiled, config):
                   input="'/usr/share/dict/web2'\n'false'\n1\n",
                   catch_exceptions=False)
 
+    # Run again, this time defaults should be shown since we have already some
+    # simulations in the database
+    runner.invoke(sem.cli, ['run', '--ns-3-path=%s' % ns_3_compiled,
+                            '--results-dir=%s' % tmpdir.join('results'),
+                            '--script=%s' % config['script']],
+                  input="\n\n1\n",
+                  catch_exceptions=False)
+
     # View all results
     runner.invoke(sem.cli, ['view', '--results-dir=%s' %
                             tmpdir.join('results'), '--show-all'],
+                  input="q",
+                  catch_exceptions=False)
+
+    # Without the simulation output
+    runner.invoke(sem.cli,
+                  ['view',
+                   '--results-dir=%s' % tmpdir.join('results'),
+                   '--show-all',
+                   '--hide-simulation-output'],
                   input="q",
                   catch_exceptions=False)
 
@@ -41,3 +87,45 @@ def test_cli_view(tmpdir, ns_3_compiled, config):
                             tmpdir.join('results')],
                   input="['/usr/share/dict/web2']\n['false']\nq",
                   catch_exceptions=False)
+
+    # Show results including simulation output
+    result = runner.invoke(sem.cli, ['view', '--results-dir=%s' %
+                                     tmpdir.join('results')])
+
+    # Get result id to test id-based options
+    result_id = re.findall('.*id\':\s\'(.*)\'', result.output)[0]
+
+    # Show results from a single simulation
+    result = runner.invoke(sem.cli, ['view', '--results-dir=%s' %
+                                     tmpdir.join('results'),
+                                     '--result-id=%s' % result_id])
+
+    # Test command printing sub-command
+    runner.invoke(sem.cli, ['command', '--results-dir=%s' %
+                            tmpdir.join('results'),
+                            result_id])
+
+    # Export results
+    runner.invoke(sem.cli, ['export', '--results-dir=%s' %
+                            tmpdir.join('results'), '--do-not-try-parsing',
+                            'matlab_output.mat'],
+                  input="\n\n1\n",
+                  catch_exceptions=False)
+    runner.invoke(sem.cli, ['export', '--results-dir=%s' %
+                            tmpdir.join('results'), '--do-not-try-parsing',
+                            'numpy_output.npy'],
+                  input="\n\n1\n",
+                  catch_exceptions=False)
+    runner.invoke(sem.cli, ['export', '--results-dir=%s' %
+                            tmpdir.join('results'), '--do-not-try-parsing',
+                            'folder_output'],
+                  input="\n\n1\n",
+                  catch_exceptions=False)
+
+    # Try using a non-existent file format
+    with pytest.raises(Exception):
+        runner.invoke(sem.cli, ['export', '--results-dir=%s' %
+                                tmpdir.join('results'), '--do-not-try-parsing',
+                                'output.fake_format'],
+                      input="\n\n1\n",
+                      catch_exceptions=False)
