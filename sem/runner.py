ad12d2cd32e25e06685bd2d63f7527edb47878c2
akshitpatel01 <akshitpatel01@gmail.com>
2021-06-19 20:09:51 +0530

    Add a function to get all valid log components and made 'environment' variable consistent

--- sem/runner.py
+++ sem/runner.py
@@ -2,6 +2,7 @@ import importlib
 import os
 import re
 import subprocess
+from sys import stdout
 import time
 import uuid
 import sem.utils
@@ -265,11 +266,37 @@ class SimulationRunner(object):
                              'SimulatorImplementationType', 'ChecksumEnabled']})
         return params  # Return a sorted list
 
+    def get_available_log_components(self):  
+        """       
+        
+        Runs a dummy simulation (only when logging is enabled) and 
+        collects all the valid log components supported by ns-3.
+        If logging is not enabled, this function is never called. 
+        """
+        ns_3_log_components = []
+        environment = {'NS_LOG': 'NonExistentLogComponent'}
+        complete_environment = {**self.environment,**environment}
+
+        try:
+            result = subprocess.check_output([self.script_executable],
+                                              env=complete_environment,
+                                              stderr=subprocess.STDOUT).decode()
+        except subprocess.CalledProcessError as e:
+            result = e.output.decode()
+
+        for log_components in result.split('\n'):
+            #TODO will this be enough to mark the end of log components
+            if (re.match(r'^msg=".+',log_components)):
+                break
+            ns_3_log_components += [log_components.split("=")[0]]
+        
+        return ns_3_log_components
+
     ######################
     # Simulation running #
     ######################
 
-    def run_simulations(self, parameter_list, data_folder, stop_on_errors=False,env=None):
+    def run_simulations(self, parameter_list, data_folder, stop_on_errors=False,environment=None):
         """
         Run several simulations using a certain combination of parameters.
 
@@ -279,16 +306,16 @@ class SimulationRunner(object):
             parameter_list (list): list of parameter combinations to simulate.
             data_folder (str): folder in which to save subfolders containing
                 simulation output.
+            environment (dict): a dictionary containing the value of NS_LOG environment variable 
+                to enable logging. 
+                Format: {'NS_LOG': 'environment_variable'}
+
+                If logging is disabled this parameter will be None.
         """
         # Add the passed environment to self.environment, which contains
             # the library path.
-        if env:
-            #TODO - this might not be the best approach, but we will need to reinitialize 
-            # the valriables called in the init() function if the user creates the campaign 
-            # in optimized mode and tried to run simulations with logging enabled
-            # Other option might be to create a new runner only for logging
-            self.__init__(self.path,self.script,False,False,self.max_parallel_processes)                                               
-            complete_environment = {**self.environment, **env}   
+        if environment:                                               
+            complete_environment = {**self.environment, **environment} 
         else:
             complete_environment = self.environment
 
@@ -349,4 +376,4 @@ class SimulationRunner(object):
             current_result['meta']['elapsed_time'] = end-start
             current_result['meta']['exitcode'] = return_code
 
-            yield current_result
+            yield current_result           
