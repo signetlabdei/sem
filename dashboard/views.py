19194ceb5cbb00eb37a88aa6ba37eb50488de5c4
akshitpatel01 <akshitpatel01@gmail.com>
2021-07-30 13:16:17 +0530

    Clean views.py

--- dashboard/views.py
+++ dashboard/views.py
@@ -11,12 +11,6 @@ def index():
 
 @tables.route("/serverside_table", methods=['GET'])
 def serverside_table_content():
-    # print('hi')
-    # db, data_dir = process_logs('/tmp/sem-test/wifi-plotting-example/data/bcbae1fc-8c50-4c61-99e7-878af4554ac8/stderr')
-    # pass_data = filter_logs(db, time_end=0)
-    # pass_data = db.table('logs').all()
-    # print(db.get(doc_id=0))
-    # print('hi')
     tab.set_request(request)
     data = tab.build_datatable()
     return jsonify(data)
@@ -24,11 +18,10 @@ def serverside_table_content():
 
 @tables.route("/filters", methods=['GET'])
 def filter():
-    un = tab.set_filter_request(request)
-    return jsonify(un)
+    tab.set_filter_request(request)
+    return jsonify('Success')
 
 
 @tables.route("/unique_values", methods=['GET'])
 def get_unique():
-    un = tab.get_unique_values(request)
     return jsonify(tab.get_unique_values(request))
