f022faa020b8963fffc6204e874c8b1c8917ddaa
akshitpatel01 <akshitpatel01@gmail.com>
2021-08-13 23:28:59 +0530

    Deleted root dashboard

--- dashboard/README.md
+++
@@ -1,4 +0,0 @@
-Steps to run dashboard:
-- Update path to Log file in __init__.py
-- python __init__.py
-- Navigate to 127.0.0.1::5000
--- dashboard/run.py
+++
@@ -1,4 +0,0 @@
-from dashboard import flask_app
-
-flask_app.config['log_path'] = '/tmp/sem-test/wifi-plotting-example/data/4b175ec0-0d1e-41bf-b6a9-b38ea8299ada/stderr'
-flask_app.run()
