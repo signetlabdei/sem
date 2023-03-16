160b486b12c7239a76678d14a8793e7647fec877
Andrea Lacava <thecave003@gmail.com>
2023-03-16 18:51:03 -0400

    improve parameters support of the callbacks
    - adaptation of parallelrunner.py to support all versions of Python
    - minor bugs fixed

--- sem/utils.py
+++ sem/utils.py
@@ -329,7 +329,6 @@ class CallbackBase(ABC):
         Initialize the callback.
         """
         self.controlled_by_parent = controlled_by_parent
-        self._init_callback()
 
     def is_controlled_by_parent(self) -> bool:
         """
@@ -343,39 +342,43 @@ class CallbackBase(ABC):
         self.n_runs_total = n_runs_total
         self._on_simulation_start()
 
+    @abstractmethod
     def _on_simulation_start(self) -> None:
         pass
 
-    def on_run_start(self) -> None:
-        self._on_run_start()
+    def on_run_start(self, sim_uuid) -> None:
+        self._on_run_start(sim_uuid)
 
-    def _on_run_start(self) -> None:
+    def _on_run_start(self, sim_uuid) -> None:
         pass
 
     @abstractmethod
     def _on_run_end(self) -> bool:
         """
         :return: If the callback returns False, training is aborted early.
+        # TODO maybe it does not make a lot of sense since this will be eventually overridden by th callback user
         """
         return True
 
     def on_run_end(self, return_code: int, sim_time: int) -> bool:
         """
         This method will be called when each simulation run finishes
+        # TODO maybe it does not make a lot of sense since this will be eventually overridden by th callback user
         :return: If the callback returns False, a run has failed.
         """
         self.n_runs_over += 1
         self.run_sim_times.append(sim_time)
-        if (return_code == 0):
-            self.n_runs_over_no_errors = 0  # type: int
+        if return_code == 0:
+            self.n_runs_over_no_errors += 1  # type: int
         else:
-            self.n_runs_over_errors = 0  # type: int
-        
-        self._on_run_end()
+            self.n_runs_over_errors += 1  # type: int
+
+        return self._on_run_end()
 
     def on_simulation_end(self) -> None:
-         self._on_simulation_end()
+        self._on_simulation_end()
 
+    @abstractmethod
     def _on_simulation_end(self) -> None:
         pass
 
